90c94a2d04b4d181ba09574f83280f1a6f896a60
Atul Gautam <atulgautamsonal3434@gmail.com>
2022-08-13 23:01:08 +0530

    updated

--- shop/views.py
+++ shop/views.py
@@ -28,16 +28,18 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, 'shop/search.html')
 
-def productview(request):
-    return HttpResponse("We are at product view")
+def productview(request,myid):
+    # fetch the product using id
+    product = Product.objects.filter(id=myid)
+    return render(request, 'shop/prodView.html',{'product':product[0]})
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request, 'shop/checkout.html')

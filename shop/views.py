ea1eb5428e8da3c01cdce521ecc794323084b8e4
Atul Gautam <atulgautamsonal3434@gmail.com>
2022-08-18 15:21:30 +0530

    updated

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 import email
 from urllib import request
 from django.shortcuts import render
-from .models import Product,Contact
+from .models import Product,Contact,Orders
 from math import ceil
 from django.http import HttpResponse
 
@@ -37,7 +37,7 @@ def contact(request):
         desc = request.POST.get('desc','')
         print(name,email,phone,desc)
         contact = Contact(name=name,email=email,phone=phone,desc=desc)
-        contact.save
+        contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
@@ -52,4 +52,19 @@ def productview(request,myid):
     return render(request, 'shop/prodView.html',{'product':product[0]})
 
 def checkout(request):
+    if request.method=="POST":
+        items_json= request.POST.get('itemsJson','')
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        address = request.POST.get('address1','')+" "+request.POST.get('address2','')
+        city = request.POST.get('city','')
+        state = request.POST.get('state','')
+        zip_code = request.POST.get('zip_code','')
+        phone = request.POST.get('phone','')
+        
+        order = Orders(name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, phone=phone, items_json=items_json)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html',{'thank':thank, 'id':id})
     return render(request, 'shop/checkout.html')
